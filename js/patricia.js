b8bc9a773d4d76fb2173b9de2f3569b8795ec341
pdo16 <patriciac-silva@hotmail.com>
2016-10-13 21:07:03 -0300

    commit aula 13-10-2016

---
+++ js/patricia.js
@@ -0,0 +1,16 @@
+var numeros = document.getElementsByClassName('number');
+
+var valorVisor = 0;
+
+
+var visor = document.getElementById('visor');
+
+for (var i in numeros) {
+	if (typeof numeros[i] !== 'object'){
+		continue;
+	}
+	numeros[i].addEventListener('click', function() {
+		alert (this.dataset.value);
+	});
+	
+}
