a974d10a4b1a2699ecf12b579ae40fa6a3607817
Bruna Moreira <bruna_moreira.andrade@hotmail.com>
2016-10-18 20:51:31 -0200

    Aula de terça - minhas modificações

--- js/bruna.js
+++ js/bruna.js
@@ -1,12 +1,73 @@
-var numeros = document.getElementsByClassName("number"); // pegra todos os numeros
-var valorVisor = 0;
-var visor = document.getElementById("visor");
+var numeros = document.getElementsByClassName("number"); // pegar todos os numeros
+var operadores = document.getElementsByClassName("operator");
+var valorTotal = 0; // valor inicial do Visor
+var clearVisor = false;
+var visor = document.getElementById("visor"); // pegando o visor
 
-for (var i in numeros) {
-	if (typeof numeros[i] !== "object") {
+for (var i in numeros) { 
+	if (typeof numeros[i] !== "object") { // se numeros[contador] !== do objeto
 		continue;
 	}
-	numeros [i].addEventListener ("click", function (){
-		alert (this.dataset.value);
-	}) //passando o loop em todos os números e adicionando um evento ao ser clicados
+	numeros[i].addEventListener ("click", function (){
+		//como eu faria para jogar o valor no visor?
+
+		if (clearVisor) {
+			visor.value = "";
+			clearVisor = false;
+		}
+		//+= é o mesmo que visor.value = visor.value + this.dataset.value;
+	  	visor.value  += this.dataset.value; // valor do visor = this (objeto global). data-value do html
+
+	}); //passando o loop em todos os números e adicionando um evento ao ser clicados
+}
+
+
+for (var i in operadores) {
+	if (typeof operadores[i] !== "object") {
+		continue;
+	}
+	operadores[i].addEventListener('click',function() {
+		//console.log (this.dataset.acao);
+		window[this.dataset.acao]();
+	})
+}
+
+//divide
+//multiplica
+//subtrai
+//soma
+//resultado
+//limpa
+
+//window['soma']()
+
+function soma() {
+	funcaoAserExecutada = function () {
+	valorTotal = parseInt(valorTotal) + parseInt(visor.value);
+	visor.value = valorTotal; 
+	clearVisor = true;
+	};
+
+	clearVisor = true;
+}
+
+function divide() {
+	alert("dividido");
+}
+function subtrai() {
+	valorTotal = parseInt(valorTotal) - parseInt(visor.value);
+	visor.value = valorTotal;
+	clearVisor = true;
+}
+
+function multiplica() {
+	alert("multiplicado");
+}
+
+function resultado() {
+	alert("resultado");
+}
+
+function limpa() {
+	alert("limpa");
 }
