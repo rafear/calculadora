feb28e4dead38492b3693e963cf67b591515806e
Alana Schueroff <alana_schueroff@hotmail.com>
2016-10-18 20:49:56 -0200

    calculadora da Alana

--- js/calculadora_alana.js
+++ js/calculadora_alana.js
@@ -1,12 +1,74 @@
 var numeros = document.getElementsByClassName('number');
-var valorVisor = 0;
+var operadores = document.getElementsByClassName('operator');
 var visor = document.getElementById('visor');
+var valorTotal = 0;
+var clearVisor = false;
+var funcaoASerExecutada = '';
 
 for (var i in numeros){
 	if (typeof numeros[i] !== 'object') {
 		continue;
 	}
 	numeros[i].addEventListener('click', function() {
-		alert(this.dataset.value);
+		if (clearVisor) {
+			visor.value = '';
+			clearVisor = false;
+		}
+		//alert(this.dataset.value);
+		visor.value += this.dataset.value;
 	});
 }
+
+for (var i in operadores) {
+	if (typeof operadores[i] !== 'object') {
+		continue;
+	}
+	operadores[i].addEventListener('click', function() {
+		window[this.dataset.acao]();
+	});
+	//window[this.dataset.acao]();
+}
+
+function dividir() {
+	valorTotal = parseInt(valorTotal) / parseInt(visor.value);
+	visor.value = valorTotal;
+	clearVisor = true;
+}
+
+function multiplicar() {
+	valorTotal = parseInt(valorTotal) * parseInt(visor.value);
+	visor.value = valorTotal;
+	clearVisor = true;
+}
+
+function subtrair() {
+	valorTotal = parseInt(valorTotal) - parseInt(visor.value);
+	visor.value = valorTotal;
+	clearVisor = true;
+}
+
+function somar() {
+	funcaoASerExecutada = function() {
+		valorTotal = parseInt(valorTotal) + parseInt(visor.value);
+		visor.value = valorTotal;
+		clearVisor = true;
+	}
+	clearVisor = true;
+}
+
+function limpartudo() {
+	resultado = 0;
+}
+
+function limpar() {
+	resultado = 0;
+}
+
+/*limpartudo
+dividir
+multiplicar
+limpar
+subtrair
+somar
+resultado
+*/
